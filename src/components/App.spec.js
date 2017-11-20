22e7d1990a7f5e6b1e67f8a067891584f9980853
Paul Massey <paul.massey@scriptwerx.io>
2017-11-20 15:40:04 +0000

    Updated tests and cleaned up

--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -1,13 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { MemoryRouter } from 'react-router-dom';
+import React, { Component } from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { Route, MemoryRouter } from 'react-router-dom';
 
 import App from './App';
 
-it('renders successfully', () => {
+const renderTestSequence = ({
+                                initialEntries,
+                                initialIndex,
+                                subject: Subject,
+                                steps
+                            }) => {
+
     const div = document.createElement('div');
-    ReactDOM.render(
-        <MemoryRouter>
-            <App />
-        </MemoryRouter>, div);
+
+    class Assert extends Component {
+
+        componentDidMount() {
+            this.assert();
+        }
+
+        componentDidUpdate() {
+            this.assert();
+        }
+
+        assert() {
+            const nextStep = steps.shift();
+            if (nextStep) {
+                nextStep({ ...this.props, div });
+            } else {
+                unmountComponentAtNode(div);
+            }
+        }
+
+        render() {
+            return this.props.children;
+        }
+    }
+
+    class Test extends Component {
+        render() {
+            return (
+                <MemoryRouter initialIndex={initialIndex} initialEntries={initialEntries}>
+                    <Route render={(props) => (
+                        <Assert {...props}>
+                            <Subject/>
+                        </Assert>
+                    )}/>
+                </MemoryRouter>
+            );
+        };
+    }
+
+    render(<Test/>, div);
+};
+
+it('navigates to Feedback', (done) => {
+
+    renderTestSequence({
+
+        subject: App,
+
+        steps: [
+
+            ({ history, div }) => {
+                console.assert(div.querySelector('h1').innerHTML.match(/National offender search/));
+                history.push('/feedback');
+            },
+
+            ({ div }) => {
+                console.assert(div.querySelector('h1').innerHTML.match(/Give feedback/));
+                done();
+            }
+        ]
+    });
 });
