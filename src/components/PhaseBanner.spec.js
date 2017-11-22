faf6db328f09511fe0d22daff84b271f92c193ee
Paul Massey <paul.massey@scriptwerx.io>
2017-11-22 14:58:08 +0000

    Fixed tests

--- src/components/PhaseBanner.spec.js
+++ src/components/PhaseBanner.spec.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
+
 import PhaseBanner from './PhaseBanner';
 
 describe('PhaseBanner', () => {
 
     it('renders successfully', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<PhaseBanner/>, div);
+        ReactDOM.render(
+            <MemoryRouter>
+                <PhaseBanner/>
+            </MemoryRouter>, div);
     });
 
 });
