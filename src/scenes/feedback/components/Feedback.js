297b1de8fdf2b86ae22cfbc2cff6647166fdff93
Paul Massey <paul.massey@scriptwerx.io>
2017-12-18 11:13:58 +0000

    Updated feedback and mail link

--- src/scenes/feedback/components/Feedback.js
+++ src/scenes/feedback/components/Feedback.js
@@ -4,7 +4,7 @@ import { Link, Redirect } from 'react-router-dom';
 
 type Props = {};
 type State = {
-  satisfaction: string,
+  impressions: string,
   feedback: string,
   doRedirect: boolean
 };
@@ -14,30 +14,30 @@ export default class Feedback extends Component<Props, State> {
     super();
 
     this.state = {
-      satisfaction: '',
+      impressions: '',
       feedback: '',
       doRedirect: false
     };
 
-    (this: any).handleRadio = this.handleRadio.bind(this);
-    (this: any).handleText = this.handleText.bind(this);
+    (this: any).handleImpressions = this.handleImpressions.bind(this);
+    (this: any).handleFeedback = this.handleFeedback.bind(this);
     (this: any).handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleRadio(event: SyntheticInputEvent<HTMLInputElement>) {
-    this.setState({ satisfaction: event.target.value });
+  handleImpressions(event: SyntheticInputEvent<HTMLInputElement>) {
+    this.setState({ impressions: event.target.value });
   }
 
-  handleText(event: SyntheticInputEvent<HTMLInputElement>) {
+  handleFeedback(event: SyntheticInputEvent<HTMLInputElement>) {
     this.setState({ feedback: event.target.value });
   }
 
   handleSubmit() {
     window.location.href =
-      'mailto:nick.gallon@digital.justice.gov.uk?subject=National%20Search%20Feedback&body=Overall%20' +
-      this.state.satisfaction +
-      '%20-%20' +
-      this.state.feedback;
+      'mailto:nick.gallon@digital.justice.gov.uk?subject=National%20Search%20Feedback&body=What%20were%20your%20first%20impressions%20of%20the%20offender%20search%3F%0D%0A%0D%0A' +
+      encodeURIComponent(this.state.impressions) +
+      '%0D%0A%0D%0AIf%20you%20could%20change%20anything%20what%20would%20it%20be%3F%0D%0A%0D%0A' +
+      encodeURIComponent(this.state.feedback);
     this.setState({ doRedirect: true });
   }
 
@@ -47,99 +47,53 @@ export default class Feedback extends Component<Props, State> {
         <h1 className="heading-xlarge">Give feedback</h1>
 
         <div className="grid-row">
-          <div className="column-one-third">
-            <nav>
-              -{' '}
-              <Link to="" className="nav-item">
-                Return to search
-              </Link>
-            </nav>
-          </div>
           <div className="column-two-thirds">
             <form onSubmit={this.handleSubmit}>
               <div className="form-group">
-                <fieldset>
-                  <legend>
-                    <span className="form-label-bold">
-                      Overall, how did you feel about the National offender
-                      search service you used today?
-                    </span>
-                  </legend>
+                <label className="form-label-bold">
+                  1. What were your first impressions of the offender search?
+                  <textarea
+                    id="impressions"
+                    name="impressions"
+                    className="form-control wide"
+                    value={this.state.impressions || ''}
+                    onChange={this.handleImpressions}
+                  />
+                </label>
 
-                  <div className="multiple-choice margin-top medium">
-                    <input
-                      id="radio-1"
-                      name="radio"
-                      type="radio"
-                      value="Very satisfied"
-                      checked={this.state.satisfaction === 'Very satisfied'}
-                      onChange={this.handleRadio}
-                    />
-                    <label htmlFor="radio-1">Very satisfied</label>
-                  </div>
-
-                  <div className="multiple-choice">
-                    <input
-                      id="radio-2"
-                      name="radio"
-                      type="radio"
-                      value="Satisfied"
-                      checked={this.state.satisfaction === 'Satisfied'}
-                      onChange={this.handleRadio}
-                    />
-                    <label htmlFor="radio-2">Satisfied</label>
-                  </div>
-
-                  <div className="multiple-choice">
-                    <input
-                      id="radio-3"
-                      name="radio"
-                      type="radio"
-                      value="Dissatisfied"
-                      checked={this.state.satisfaction === 'Dissatisfied'}
-                      onChange={this.handleRadio}
-                    />
-                    <label htmlFor="radio-3">Dissatisfied</label>
-                  </div>
-
-                  <div className="multiple-choice">
-                    <input
-                      id="radio-4"
-                      name="radio"
-                      type="radio"
-                      value="Very dissatisfied"
-                      checked={this.state.satisfaction === 'Very dissatisfied'}
-                      onChange={this.handleRadio}
-                    />
-                    <label htmlFor="radio-4">Very dissatisfied</label>
-                  </div>
-                </fieldset>
-
-                <hr />
+                <div className="margin-top large">&nbsp;</div>
 
                 <label className="form-label-bold">
-                  How could we improve this service?
+                  2. If you could change anything what would it be?
                   <textarea
                     id="feedback"
                     name="feedback"
                     className="form-control wide"
-                    placeholder="Start typing here..."
-                    type="text"
                     value={this.state.feedback || ''}
-                    onChange={this.handleText}
+                    onChange={this.handleFeedback}
                   />
                 </label>
               </div>
 
+              <p className="no-margin-bottom">
+                This is a very early stage test for a new kind of National
+                Offender Search.
+              </p>
+              <p>
+                This test does not use any real offender data or store any data
+                in any way.
+              </p>
+
               <div className="margin-top large">&nbsp;</div>
 
-              <button className="button button-start" type="submit">
-                Submit feedback
+              <button className="button" type="submit">
+                Send feedback
               </button>
             </form>
 
             {this.state.doRedirect && <Redirect to="/" />}
           </div>
+          <div className="column-one-third">&nbsp;</div>
         </div>
       </div>
     );
