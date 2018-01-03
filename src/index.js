e71efc30750eb10a04e3b60ab209080cae4be2b3
Paul Massey <paul.massey@scriptwerx.io>
2018-01-03 13:28:04 +0000

    Polyfill

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 /* polyfills */
-import 'core-js/fn/array/from';
-import 'core-js/fn/array/find-index';
-import 'core-js/fn/string/includes';
+import 'babel-polyfill';
 
 import React from 'react';
 import { render } from 'react-dom';
