6a14aa79511314eb5a12a392f17dea136039b67f
Paul Massey <paul.massey@scriptwerx.io>
2018-01-16 15:18:57 +0000

    Expanded search query

--- src/scenes/search/data/Query.js
+++ src/scenes/search/data/Query.js
@@ -20,19 +20,33 @@ const Query = (
               fields: [
                 'CRN^10',
                 'NOMS_NUMBER^8',
-                'NI_NUMBER',
+                'NI_NUMBER^6',
                 'FIRST_NAME^10',
-                'SURNAME^10',
                 'SECOND_NAME^4',
                 'THIRD_NAME^4',
+                'SURNAME^10',
                 'PREVIOUS_SURNAME^6',
+                'DATE_OF_BIRTH_DATE^5',
                 'ALIASES.FIRST_NAME^6',
                 'ALIASES.SURNAME^6',
-                'DATE_OF_BIRTH_DATE^5',
-                'ALIASES.SECOND_NAME^3',
-                'ALIASES.THIRD_NAME^3',
+                'ALIASES.DATE_OF_BIRTH_DATE^5',
+                'ADDRESSES.POSTCODE^3',
+                'ALIASES.SECOND_NAME^2',
+                'ALIASES.THIRD_NAME^2'
+              ]
+            }
+          },
+          {
+            multi_match: {
+              query: searchParams,
+              type: 'best_fields',
+              operator: 'or',
+              fields: [
+                'ADDRESSES.STREET_NAME',
                 'ADDRESSES.TOWN_CITY',
-                'ADDRESSES.POSTCODE^3'
+                'ADDRESSES.COUNTY',
+                'E_MAIL_ADDRESS',
+                'MOBILE_NUMBER'
               ]
             }
           }
