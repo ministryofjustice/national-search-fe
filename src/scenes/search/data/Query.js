c49f0aab4cce9d3ba3d9cfb57fe72753d42e29b5
Paul Massey <paul.massey@scriptwerx.io>
2018-01-16 15:30:53 +0000

    Updates to query

--- src/scenes/search/data/Query.js
+++ src/scenes/search/data/Query.js
@@ -18,33 +18,33 @@ const Query = (
               type: 'cross_fields',
               operator: 'and',
               fields: [
-                'CRN^10',
-                'NOMS_NUMBER^8',
-                'NI_NUMBER^6',
                 'FIRST_NAME^10',
+                'SURNAME^10',
                 'SECOND_NAME^4',
                 'THIRD_NAME^4',
-                'SURNAME^10',
                 'PREVIOUS_SURNAME^6',
-                'DATE_OF_BIRTH_DATE^5',
-                'ALIASES.FIRST_NAME^6',
-                'ALIASES.SURNAME^6',
-                'ALIASES.DATE_OF_BIRTH_DATE^5',
-                'ADDRESSES.POSTCODE^3',
-                'ALIASES.SECOND_NAME^2',
-                'ALIASES.THIRD_NAME^2'
+                'ALIASES.FIRST_NAME^8',
+                'ALIASES.SURNAME^8',
+                'ALIASES.SECOND_NAME^3',
+                'ALIASES.THIRD_NAME^3'
               ]
             }
           },
           {
             multi_match: {
               query: searchParams,
-              type: 'best_fields',
+              type: 'most_fields',
               operator: 'or',
               fields: [
+                'CRN^10',
+                'NOMS_NUMBER^8',
+                'NI_NUMBER^6',
+                'DATE_OF_BIRTH_DATE^5',
+                'ALIASES.DATE_OF_BIRTH_DATE^5',
                 'ADDRESSES.STREET_NAME',
                 'ADDRESSES.TOWN_CITY',
                 'ADDRESSES.COUNTY',
+                'ADDRESSES.POSTCODE^3',
                 'E_MAIL_ADDRESS',
                 'MOBILE_NUMBER'
               ]
